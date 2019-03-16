388aed88151d0250a36f185cf0dc472fb943cd44
Aamir Kalimi <aamirkalimi.edu@gmail.com>
2019-03-16 13:47:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 function breakOut(array, changeValue, stopValue){
   for (let i = 0; i < array.length; i++) {
-    array[i] = changeValue;
-    if (changeValue == stopValue) {
+    if (array[i] == stopValue) {
       break;
+    } else {
+      array[i] = changeValue;
     }
   }
 }
