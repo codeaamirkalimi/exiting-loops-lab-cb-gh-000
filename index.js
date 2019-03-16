51af4c6355d46ec4f0d9b72ea4e4908aff4c0f24
Aamir Kalimi <aamirkalimi.edu@gmail.com>
2019-03-16 13:43:18 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,9 @@
+function breakOut(array, changeValue, stopValue){
+  for (let i = 0; i < array.length; i++) {
+    if (changeValue == stopValue) {
+      break;
+    } else {
+      changeValue = array[i];
+    }
+  }
+}
